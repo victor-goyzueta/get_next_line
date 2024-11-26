7fee677ca69713cb6563978466a9bb288b822fc7
Víctor J. Goyzueta <167643295+victor-goyzueta@users.noreply.github.com>
2024-11-26 01:18:29 +0100

    update

--- get_next_line.c
+++ get_next_line.c
@@ -6,30 +6,30 @@
 /*   By: vgoyzuet <vgoyzuet@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/25 23:10:26 by vgoyzuet          #+#    #+#             */
-/*   Updated: 2024/11/26 01:13:15 by vgoyzuet         ###   ########.fr       */
+/*   Updated: 2024/11/26 01:17:54 by vgoyzuet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-char *buffer_read(fd)
+char	*buffer_read(fd)
 {
-    //
+	//
 }
 
-static  *line_read(fd, line)
+static	*line_read(fd, line)
 {
-    if (!line)
-        malloc(1);
-        line = "";
-    while (ft_strchr(line, '\n'))
-        read(fd, *buffer_read, BUFFER_SIZE);
+	if (!line)
+		malloc(1);
+	line = "";
+	while (ft_strchr(line, '\n'))
+		read(fd, *buffer_read, BUFFER_SIZE);
 }
 
 char	*get_next_line(int fd)
 {
-    char    *line;
-    
-    line_read(fd);
-    return (line_read);
+	char	*line;
+
+	line_read(fd);
+	return (line_read);
 }

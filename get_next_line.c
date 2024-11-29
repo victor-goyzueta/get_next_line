48be5c41f6a2fae318645b379f5a4d961552c9d4
Víctor J. Goyzueta <167643295+victor-goyzueta@users.noreply.github.com>
2024-11-29 01:23:09 +0100

    update

--- get_next_line.c
+++ get_next_line.c
@@ -6,11 +6,13 @@
 /*   By: vgoyzuet <vgoyzuet@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/25 23:10:26 by vgoyzuet          #+#    #+#             */
-/*   Updated: 2024/11/28 22:14:37 by vgoyzuet         ###   ########.fr       */
+/*   Updated: 2024/11/29 01:22:50 by vgoyzuet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
 char	*get_next_line(int fd)
-{}
+{
+	
+}

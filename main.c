af3faa4cf99ea7c1d01adb0399a96da6676539a1
victor-goyzueta <vgoyzuet@c2r14s4.42madrid.com>
2024-12-05 19:01:47 +0100

    update main_bonus

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: vgoyzuet <vgoyzuet@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/25 23:34:47 by vgoyzuet          #+#    #+#             */
-/*   Updated: 2024/12/05 17:17:44 by vgoyzuet         ###   ########.fr       */
+/*   Updated: 2024/12/05 18:58:44 by vgoyzuet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,11 +17,9 @@ int	main()
 	int		fd;
 	char	*line;
 
-	fd = open("file.txt", O_RDONLY);
+	fd = open("file1.txt", O_RDONLY);
 	if (fd == -1)
 		return (perror("Error opening the file"), 1);
-	line = get_next_line(fd);
-	printf("%s", line);
 	while ((line = get_next_line(fd)) != NULL)
 	{
 		printf("%s", line);
--- main_bonus.c
+++ main_bonus.c
@@ -6,28 +6,60 @@
 /*   By: vgoyzuet <vgoyzuet@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/05 17:25:37 by vgoyzuet          #+#    #+#             */
-/*   Updated: 2024/12/05 17:25:40 by vgoyzuet         ###   ########.fr       */
+/*   Updated: 2024/12/05 18:54:58 by vgoyzuet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-int	main()
+int	all_read( int *fd_finish, int argn)
 {
-	int		fd;
-	char	*line;
-
-	fd = open("file.txt", O_RDONLY);
-	if (fd == -1)
-		return (perror("Error opening the file"), 1);
-	line = get_next_line(fd);
-	printf("%s", line);
-	while ((line = get_next_line(fd)) != NULL)
+	while(argn--)
 	{
-		printf("%s", line);
-		free(line);
+		if (fd_finish[argn] == 0)
+			return (0);
 	}
-	if (close(fd) == -1)
-		return (perror("Error closing the file"), 1);
+	return (1);
+}
+
+int	main(int argc, char *argv[])
+{
+	int		fd[argc - 1];
+	int		fd_finish[argc - 1];
+	char	*line;
+	int     i;
+
+	i = 0;
+	while (i < (argc - 1))
+	{
+		fd[i] = open(argv[i + 1], O_RDONLY);
+		if (fd[i] == -1)
+			return (perror("Error opening the file"), 1);
+		fd_finish[i] = 0;
+		i++;
+	}
+	while (1)
+	{
+		i = 0;
+		while (i < argc - 1)
+		{
+			line = get_next_line(fd[i]);
+			if (line)
+				printf("file [%i] line: %s", fd[i], line);
+			else
+				fd_finish[i] = 1;
+			free(line);
+			i++;
+		}
+		if (all_read(fd_finish, argc - 1))
+			break ;
+	}
+	i = 0;
+	while (i < argc - 1)
+	{
+		if (close(fd[i]) == -1)
+			return (perror("Error closing the file"), 1);
+		i++;
+	}
 	return (0);
 }
